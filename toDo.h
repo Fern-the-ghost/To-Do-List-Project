c8e0754a2af6976662e3fbf263f4f0b23662aba0
Fernanda Padilla <W1907532@apps.losrios.edu>
2025-06-04 12:51:58 -0700

    Update toDo.h

--- toDo.h
+++ toDo.h
@@ -1,1 +1,16 @@
 //will be the header file for ToDoList
+#ifndef TODO_H
+#define TODO_H
+
+Class toDo {
+  public:
+//adding function
+//deleting function
+//view function
+  private:
+//private vector
+//private string
+//etc...
+};
+
+#endif
