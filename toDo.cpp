0056953fe249a74498aca4329839ca29c0c09e29
Fernanda Padilla <W1907532@apps.losrios.edu>
2025-06-05 10:11:37 -0700

    Update toDo.cpp

--- toDo.cpp
+++ toDo.cpp
@@ -1,1 +1,19 @@
 //Will be the cpp file to my header file where I will write out the functions
+#include "toDo.h"
+#include <iostream>
+#include <vector>
+
+void add()
+{
+  //add function
+}
+
+void delete()
+{
+  //delete function
+}
+
+void view()
+{
+  //view function
+}
